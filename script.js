ee601c52d089c073c248a8a1b18ccdc4918db0e3
Isar <isar.arnautovic16@gmail.com>
2022-06-09 13:01:07 +0200

    Working on DOM elements

--- script.js
+++ script.js
@@ -23,5 +23,44 @@ let myLibrary = [];
 let newBook;
 
 function addBookToLibrary() {
+    event.preventDefault();
+    newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 
+}
+
+// DOM Elements
+function domBook(item) {
+    const library = document.querySelector('book-container');
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pagesDiv = document.createElement('div');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    bookDiv.classList.add('.book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
+
+    authorDiv.textContent = item.author;
+    authorDiv.classList.add('author');
+    bookDiv.appendChild(author);
+
+    pagesDiv.textContent = item.pages;
+    pagesDiv.classList.add('pages');
+    bookDiv.appendChild(pagesDiv);
+
+    readBtn.classList.add('readBtn');
+    bookDiv.appendChild(readBtn);
+    if (item.read === false) {
+        readBtn.textContent = 'Not Read';
+    } else {
+        readBtn.textContent = 'Read';
+    }
+
+    library.appendChild(bookDiv);
 }
